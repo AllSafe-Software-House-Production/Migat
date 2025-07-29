95f6069a25eb87154e66df64834d91b34d023d00
alls4f3-front <all.safe0dev@gmail.com>
2025-07-29 15:22:48 +0300

    enhacen

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -14,31 +14,31 @@ class Hotel extends Model implements HasMedia
         'distance_from_haram', 'special_check_in_instructions', 'access_methods',
         'pets', 'commission', 'commercial_register', 'tourism_license',
         'utility_bill', 'owner_id', 'ipan', 'visa', 'number_of_rooms',
-        'phone', 'email', 'service_ids', 'policy_ids',
+        'phone', 'email', 'service', 'policy', 'hotel_link',
     ];
 
     protected $casts = [
         'photos' => 'array',
-        'service_ids' => 'array',
-        'policy_ids' => 'array',
+        // 'service_ids' => 'array',
+        // 'policy_ids' => 'array',
         'availability' => 'boolean',
     ];
-
+    
 
     public function owner()
     {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function services()
-    {
-        return $this->hasMany(Service::class, 'id', 'service_ids');
-    }
+    // public function services()
+    // {
+    //     return $this->hasMany(Service::class, 'id', 'service_ids');
+    // }
 
-    public function policies()
-    {
-        return $this->hasMany(Policy::class, 'id', 'policy_ids');
-    }
+    // public function policies()
+    // {
+    //     return $this->hasMany(Policy::class, 'id', 'policy_ids');
+    // }
 
     public function rooms()
     {
@@ -50,10 +50,5 @@ class Hotel extends Model implements HasMedia
         $this->addMediaCollection('images');
         $this->addMediaCollection('utility_bill');
 
-    }
-
-    public function favorites()
-    {
-        return $this->hasMany(Favorite::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -49,3 +49,29 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('reviews', [ReviewController::class, 'store']);
     Route::post('logout', [AuthController::class, 'logout']);
 });
+
+    Route::get('/run-migrations', function (Request $request) {
+        $key = $request->query('key');
+
+        if ($key !== 'hhhlol') {
+            abort(403);
+        }
+
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Migration done.';
+    });
+
+    Route::get('/run-seeder', function (Request $request) {
+        $key = $request->query('key');
+
+        if ($key !== 'hhhlol') {
+            abort(403, 'Unauthorized');
+        }
+
+        Artisan::call('db:seed', [
+            '--class' => 'AdminPermissionsSeeder',
+            '--force' => true,
+        ]);
+
+        return 'Seeding done.';
+    });
